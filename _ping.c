780f7f0ed6eae0f278cbe890dbb38cf9e68c286d
techlinked <techlinked@yandex.ru>
2019-04-14 19:17:50 +0500

    created

---
+++ _ping.c
@@ -0,0 +1,119 @@
+/*
+ * _ping.c
+ *
+ *  Created on: 11 апр. 2019 г.
+ *      Author: a.khakimov
+ */
+
+#include "proto.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip_icmp.h>
+
+unsigned short checksum(void *b, int len);
+
+#define PING_PKT_SZ 	64
+
+typedef struct {
+	struct icmp hdr;
+	char data[PING_PKT_SZ - sizeof(struct icmp)];
+} ping_pkt_t;
+
+/* _ping - Check the availability of the communication partner with a ping request.
+ * ip - IP address of the communication partner as string
+ * timeout - Timeout in milliseconds to wait until reply
+ * reply_time - Pointer to get average reply time of the ping request in milliseconds
+ */
+
+int _ping(devctl_ping_t* p)
+{
+
+	printf("_ping: ip=%s, timeout=%lu \n", p->args.ip, p->args.timeout);
+	int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+	struct sockaddr_in addr_con;
+	addr_con.sin_family = AF_INET;
+	inet_aton(p->args.ip, (struct in_addr*) &addr_con.sin_addr.s_addr);
+	socklen_t socklen = sizeof(addr_con);
+
+	ping_pkt_t ping_pkt;
+	memset(&ping_pkt, 0, sizeof(ping_pkt));
+
+	int i = 0;
+	for(; i < sizeof(ping_pkt.data) - 1; ++i) {
+		ping_pkt.data[i] = i + '0';
+	}
+	ping_pkt.data[i] = 0;
+
+	ping_pkt.hdr.icmp_type = ICMP_ECHO;
+	srand(time(NULL));
+	int random_id = rand();
+	ping_pkt.hdr.icmp_hun.ih_idseq.icd_id = random_id;
+	ping_pkt.hdr.icmp_hun.ih_idseq.icd_seq = 0;
+	ping_pkt.hdr.icmp_cksum = checksum(&ping_pkt, sizeof(ping_pkt));
+
+	struct timespec time_start, time_end;
+	clock_gettime(CLOCK_MONOTONIC, &time_start);
+
+	/* This parameter should be set for SOL_IP, but in QNX this parameter is not defined
+	    int ttl_val = 64;
+	    if(setsockopt(sock, SOL_SOCKET, IP_TTL, &ttl_val, sizeof(ttl_val))) {
+	    	printf("Setting socket TTL failed \n");
+	    	return 1;
+	    }
+	 */
+
+	unsigned long wait_time_ms = p->args.timeout;
+	struct timespec wait_time;
+	wait_time.tv_sec = wait_time_ms / 1000;
+	wait_time.tv_nsec = wait_time_ms * 1000000;
+	if(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &wait_time, sizeof(wait_time))) {
+		perror("Setting socket wait time failed \n");
+		p->result.return_ = 1;
+		return 0;
+	}
+
+	if(sendto(sock, &ping_pkt, PING_PKT_SZ, 0, (struct sockaddr*)&addr_con, socklen) <= 0) {
+		printf("Send echo failed \n");
+		p->result.return_ = 1;
+		return 0;
+	}
+
+	struct sockaddr_in r_addr;
+	if(recvfrom(sock,  &ping_pkt, PING_PKT_SZ, 0, (struct sockaddr*)&r_addr, &socklen) <= 0) {
+		perror("Get reply failed \n");
+		p->result.return_ = 1;
+		return 0;
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &time_end);
+
+	const unsigned long start_time_ms = time_start.tv_sec * 1000 + (time_start.tv_nsec / 1000000);
+	const unsigned long end_time_ms = time_end.tv_sec * 1000 + (time_end.tv_nsec / 1000000);
+	p->result.reply_time = end_time_ms - start_time_ms;
+	p->result.return_ = 0;
+	printf("_ping return: ip=%s, reply_time=%lu \n", p->args.ip, p->result.reply_time);
+	return sizeof(p->result);
+}
+
+unsigned short checksum(void *b, int len)
+{
+	unsigned short *buf = b;
+	unsigned int sum=0;
+	unsigned short result;
+
+	for ( sum = 0; len > 1; len -= 2 )
+		sum += *buf++;
+	if ( len == 1 )
+		sum += *(unsigned char*)buf;
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	result = ~sum;
+	return result;
+}
+
+
