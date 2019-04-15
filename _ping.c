65f04a72819d2d1312e1b9b19881fa6dadf5f79b
techlinked <techlinked@yandex.ru>
2019-04-15 12:21:46 +0500

    fix timeout bug

--- _ping.c
+++ _ping.c
@@ -8,7 +8,7 @@
 #include "proto.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <stdbool.h>
 #include <string.h>
 #include <arpa/inet.h>
@@ -37,11 +37,7 @@ unsigned long get_cur_time_ms();
 unsigned short checksum(void *b, int len);
 
 /* _ping - Check the availability of the communication partner with a ping request.
- * ip - IP address of the communication partner as string
- * timeout - Timeout in milliseconds to wait until reply
- * reply_time - Pointer to get average reply time of the ping request in milliseconds
  */
-
 int _ping(devctl_ping_t* p)
 {
 	char ip[IPSTR_MAXLEN];
@@ -81,9 +77,9 @@ bool set_socket_rcv_timeout(int sock, const unsigned long timeout)
 {
 	bool result = true;
 	unsigned long wait_time_ms = timeout;
-	struct timespec wait_time;
+	struct timeval wait_time;
 	wait_time.tv_sec = wait_time_ms / 1000;
-	wait_time.tv_nsec = wait_time_ms * 1000000;
+	wait_time.tv_usec = (wait_time_ms % 1000) * 1000;
 	if(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &wait_time, sizeof(wait_time))) {
 		result = false;
 	}
